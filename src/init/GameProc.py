8933da4b51447e94c5436f21616bdeacf203ab97
Nikita2005qwe <Nikitaborakov@gmail.com>
2024-12-07 16:02:03 +0700

    apdate GameProc and Gameogcc

---
+++ src/init/GameProc.py
@@ -0,0 +1,11 @@
+
+class GameProc:
+    def __init__(self):
+        self.Play = True
+        self.tick_rate = 30
+    
+    def exit_game(self):
+        pass
+
+    def update(self):
+        pass
